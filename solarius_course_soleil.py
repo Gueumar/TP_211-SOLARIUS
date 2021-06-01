3ee18d92fd9c138d52323346709b8e9744063e78
Margaux <84778994+Gueumar@users.noreply.github.com>
2021-06-01 14:03:30 +0200

    Add files via upload

---
+++ solarius_course_soleil.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#  -- coding: utf-8 --
+
+# Date: Tue 01 June 2021 14:04:28 CET
+# Author: Margaux Fouque
+
+# Description:
+# SOLARIUS est un outil technique permettant de suivre la course du soleil
+# tout au long de la journée.
+
+
+# Python Version 3.9
+# ECAM - AAM 1
+
+
+''' Contrôle d'un servomoteur avec un Raspberry Pi
+Le programme demande à l'utilisateur d'entrer le rapport cyclique
+(duty cycle) désiré, et le servomoteur se met à la position correspondante. '''
