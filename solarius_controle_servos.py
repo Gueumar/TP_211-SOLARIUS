54d4ea5267df4846bef011e8cc2c877b8170a519
Margaux <84778994+Gueumar@users.noreply.github.com>
2021-06-01 14:02:21 +0200

    Add files via upload

---
+++ solarius_controle_servos.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+#  -- coding: utf-8 --
+
+# Date: Tue 25 May 2021 14:14:38 CET
+# Author: Margaux Fouque
+
+# Description:
+# SOLARIUS est un outil technique permettant de suivre la course du soleil
+# tout au long de la journée.
+# SOLARIUS doit rester à la même postion au cours du processus.
+
+# Python Version 3.9
+# ECAM - AAM 1
+
+
+# pseudo code permettant aux servos de tourner
+
+
+4 capteurs différents 
+servos tournent et changent position panneau selon luminosité captée
+
+si capteur1 cpx capte
+
+	m^me luminosité -> servo reste m^me position
+
+def rotation(position, capteur):
+''' La fonction rotation permet de donner au servo l'angle 
+entre sa positon initiale et la position choisie '''
+
